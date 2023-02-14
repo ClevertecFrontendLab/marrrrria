889dfe4e6b39696750c86d76168a233079deff72
marrrrria <valzyr48@gmail.com>
2023-02-14 11:10:12 +0300

    feat: add loader

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,5 +1,7 @@
 export function Loader() {
+
+
   return (
-    <p style={{color: 'white', fontSize: '30px', position: 'absolute', backgroundColor: '#99999999', top: '0', left: '0', right: '0', bottom: '0', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>Loading...</p>
+    <div className="loader"><span className="ico ico_loader loader__icon"> </span></div>
   )
 }
